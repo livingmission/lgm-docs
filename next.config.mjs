503d152ebf06acb265e7f35535741e8908dfbe5d
avenbreaks <dev-cosmware@gitshock.com>
2026-01-19 12:05:18 +0700

    feat: create docs lgm

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,12 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Uncomment and change this if your repo name is not the root
+  // basePath: '/your-repo-name',
 };
 
 export default withMDX(config);
